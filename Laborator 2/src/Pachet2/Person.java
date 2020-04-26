63cfc85e5ce9d10e2dc99059c24e83257e4c0069
GabrielaDragan <37863865+GabrielaDragan@users.noreply.github.com>
2020-04-26 23:16:33 +0300

    Laboratorul 2

---
+++ Laborator 2/src/Pachet2/Person.java
@@ -0,0 +1,66 @@
+package Pachet2;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private String idNo;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Person(){
+    }
+
+    public boolean changeType() {
+        boolean changed = false;
+        if(type.equals("teacher")){
+            type = "doctor";
+            changed = true;
+        }
+        return changed;
+    }
+
+    public boolean changeTypeIsTheSame(String type){
+        return this.type.equals(type);
+    }
+
+}
